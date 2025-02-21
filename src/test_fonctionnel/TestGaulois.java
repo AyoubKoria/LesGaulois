ef84391149348d8193e827173cfde86d981e990c
Ayoub Koriaa <krb3611a@ensfsi.onmicrosoft.com>
2025-02-21 09:21:45 +0100

    Toutes les questions faites jusqu'au 3.2

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,5 +1,24 @@
 package test_fonctionnel;
 
+import personnages.Gaulois;
+import personnages.Romain;
+
 public class TestGaulois {
+	public static void main(String[] args) {
+		System.out.println();
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obélix", 16);
+
+		asterix.parler("Bonjour Obélix.");
+		obelix.parler("Bonjour Astérix. Ca te dirais d'aller chasser des sangliers ?");
+		asterix.parler("Oui très bonne idée.");
+
+		Romain maximus = new Romain("Maximus", 10);
+
+		asterix.frapper(maximus);
+		asterix.frapper(maximus);
+		asterix.frapper(maximus);
+
+	}
 
 }
